23eead021e7dcfab3746d4b683c555acf040994b
Enrique Aliaga <e.aliaga.chavez@gmail.com>
2018-01-09 20:44:27 -0500

    Chapter 1: Quiz

---
+++ Quiz/Quiz/BNRAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Quiz
+//
+//  Created by Enrique Aliaga on 10/28/17.
+//  Copyright © 2017 Big Nerd Ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Quiz/Quiz/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Quiz
+//
+//  Created by Enrique Aliaga on 10/28/17.
+//  Copyright © 2017 Big Nerd Ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "BNRAppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BNRAppDelegate class]));
+    }
+}
